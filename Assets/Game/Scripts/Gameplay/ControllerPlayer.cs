c677f2304deb64655e2144205c40cddad2b24fd7
naumnek <naumnek@yandex.ru>
2021-04-01 17:32:46 +0300

    Update ControllerPlayer.cs

--- Assets/Game/Scripts/Gameplay/ControllerPlayer.cs
+++ Assets/Game/Scripts/Gameplay/ControllerPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControllerPlayer : MonoBehaviour
 {
@@ -45,7 +46,7 @@ public class ControllerPlayer : MonoBehaviour
         }
 		if (Input.GetKeyDown(KeyCode.Escape))
         {
-             Application.Quit();
+             SceneManager.LoadScene("MainMenu");
         }
     }
 
